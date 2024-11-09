c5a7673fb6c18ac6daacd8a8a91d2a8218f4f0b1
Sajid Ali Khan <skprogramminghi@gmail.com>
2024-11-09 13:29:34 +0500

    lab 10 about seaborn libray added

---
+++ lab 10/practice.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+# print(sns.get_dataset_names())
+
+dataset  = sns.load_dataset('anscombe')
+# print(dataset)
+
+# print(dataset.head(1))
+# print(dataset.tail(2))
+
+# print(dataset.axes[1:2][0][0])
+
